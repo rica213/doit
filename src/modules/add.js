957513928eed3be6644c27dadaedcb238afb2abd
rica213 <25f5k252l@mozmail.com>
2023-01-27 14:29:51 +0300

    Fix the function add

--- src/modules/add.js
+++ src/modules/add.js
@@ -1,17 +1,15 @@
-/* eslint-disable no-else-return */
 import Task from './task.js';
 import { newTask } from './taskElements.js';
 import currentTasks from './tasks.js';
 
 const add = (event) => {
+  const task = new Task(newTask.value, false, currentTasks.tasks.length + 1);
   if (newTask.value === '') {
     event.preventDefault();
-    return false;
   } else {
-    const task = new Task(newTask.value, false, currentTasks.tasks.length + 1);
     newTask.value = '';
-    return task;
   }
+  return task;
 };
 
 export default add;
