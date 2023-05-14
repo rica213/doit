78dfc09e31689766166282b40524709ed431d5d2
rica213 <25f5k252l@mozmail.com>
2023-05-14 23:22:27 +0300

    Refactor taskList entity

--- src/domain/entities/taskList.js
+++ src/domain/entities/taskList.js
@@ -1,4 +1,6 @@
 /**
+ * Create a new instance of taskList
+ * @constructor
  * @param {String} description
  * @param {Integer} index
  * @returns {TaskList}
@@ -11,81 +13,38 @@ class TaskList {
   }
 
   /**
-   * Getter for the instance variable description
+   * Returns the description of the taskList
    * @returns {String} description
    */
   getDescription = () => this.description;
 
   /**
-   * Getter for the instance variable index
+   * Returns the index of the taskList
    * @returns {Integer} index
    */
   getIndex = () => this.index;
 
   /**
-   * Getter for the attribute tasks
-   * @returns {Task[]} tasks
+   * Returns the list of tasks in the taskList
+   * @returns {<Array>Task} tasks
    */
   getListTasks = () => this.tasks;
 
   /**
-   * Add a new task to the task list
+   * Adds a new task to the task list
    * @param {Task} task
    */
   addTask = (task) => {
     this.tasks.concat(task);
   };
 
-  /** 
-   * Set description of the taskList
+  /**
+   * Sets a new description to the taskList
    * @param {String} description
   */
   setDescription = (description) => {
     this.description = description;
   }
-
-
-/*   update = (desc, id) => {
-    this.tasks[id].description = desc;
-  };
-
-  init = (element) => {
-    element.innerHTML = "";
-  };
-
-  display = (element) => {
-    this.tasks.forEach((task, index) => {
-      const taskItem = document.createElement("li");
-      taskItem.id = index;
-      taskItem.innerHTML = `
-      <input type="checkbox" id="task-${task.index}" name="task-${
-        task.index
-      }" ${task.completed ? "checked" : "unchecked"}>
-      <p contenteditable="true" class="description">${task.description}</p>
-      <i class="fa fa-ellipsis-v"><br>
-      `;
-      element.appendChild(taskItem);
-    });
-  };
-
-  deleteAllCompleted = () => {
-    this.tasks = this.tasks.filter((task) => task.completed === false);
-  };
-
-  updateIndex = () => {
-    this.tasks.forEach((task, index) => {
-      task.index = index + 1;
-    });
-  };
-
-  delete = (index) => {
-    this.tasks = this.tasks.filter((task) => task.index !== Number(index) + 1);
-  };
-
-  complete = (index, status) => {
-    this.tasks[index].completed = status;
-  }; */
 }
 
-// const currentTasks = new Tasks();
-export default Tasks;
+export default TaskList;
